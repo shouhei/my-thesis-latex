af07425cb447962fe6d8846594c76e6f56b46696
山口将平 <shouhei.yamaguchi.be@gmail.com>
2016-01-05 10:54:14 +0900

    fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
 var gulp = require('gulp');
 var exec = require('child_process').exec;
+var notifier = require('node-notifier');
 
 gulp.task('tex', function(){
-  gulp.watch('./src/**/*.tex', ['build']);
+    gulp.watch(['./src/**/*.tex'], ['build']);
+    gulp.watch(['./src/**/*.bib'], ['build']);
+    gulp.watch(['./src/**/*.png'], ['build']);
+    gulp.watch(['./src/**/*.jpg'], ['build']);
 });
 gulp.task('build', function(){
   exec('./bin/mklatex', function (err, stdout, stderr) {
       console.log(stdout);
       console.log(stderr);
+      notifier.notify({
+          title: 'gulp tex build',
+          message: 'success',
+          sound: true,
+          wait: true
+      }, function (err, response) {
+      });
   });
 });
